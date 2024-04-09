cad46714107e20e5f05934c21ed632db997a99ab
okoji Kelechi <kelechiokoji52@gmail.com>
2024-04-09 21:07:55 +0100

    Added observer for test

--- __tests__/units/components/home.test.tsx
+++ __tests__/units/components/home.test.tsx
@@ -1,10 +1,37 @@
-// import DashboardPage from "@/app/(protected)/dashboard/page";
+import LoginPage from "@/app/(auth)/login/page";
+import Home from "@/app/page";
+import { render, screen } from "@testing-library/react";
 
-// import { render, screen } from "@testing-library/react";
-// describe("Home", () => {
-//   it("Should render correctly", () => {
-//     render(<DashboardPage />);
-//     const H1Element = screen.getByRole("button");
-//     expect(H1Element).toBeInTheDocument();
-//   });
-// });
+export class IntersectionObserver {
+  root = null;
+  rootMargin = "";
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+window.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver;
+
+describe("Home", () => {
+  it("Should render correctly", () => {
+    render(<Home />);
+    const H1Element = screen.getByRole("link", {
+      name: "Login",
+    });
+    expect(H1Element).toBeInTheDocument();
+  });
+});
